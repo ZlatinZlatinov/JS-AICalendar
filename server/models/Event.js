d826129059420e36ebd61e2772af26b763ef665f
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-06-18 23:55:43 +0300

    Created Event model

---
+++ server/models/Event.js
@@ -0,0 +1,48 @@
+const {Schema, model} = require('mongoose'); 
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String,
+        required: true
+    }, 
+    location: {
+        type: String,
+        required: true
+    }, 
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String
+    },
+    //Not sure about this one
+    freeSlots: {
+        type: Number,
+        required: true
+    },
+    //Not sure about this one too
+    dateRange: {
+        type: String
+    }, 
+    createdAt: {
+        type: String,
+        required: true,
+        default: Date.now()
+    },
+    ownerId: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    participants: [{type: Schema.Types.ObjectId, ref: 'User'}]
+}); 
+
+const Event = model('Event', eventSchema); 
+
+module.exports = {
+    Event
+}
