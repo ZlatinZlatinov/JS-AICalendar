fbcbd0818c00eb383f2cde6a6d9b5ecea45def32
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-06-19 15:42:00 +0300

    Updated authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,13 +1,29 @@
-const authController = require('express').Router(); 
+const { hasUser } = require('../middlewares/guard');
+const { loginUser, logout } = require('../services/userService');
+const { erorParser } = require('../utils/errorParser');
+
+const authController = require('express').Router();
 
 //User login
-authController.post('/login', (req, res) => {
+authController.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-}); 
+    try {
+        const token = await loginUser({ email, password });
+        res.json({ accessToken: token });
+    } catch (err) {
+        const message = erorParser(err);
+        res.status(401).json({ message });
+    }
+});
 
 //User logout
-authController.get('/logout', (req, res)=> {
-    
+authController.post('/logout', hasUser(), (req, res) => {
+    const token = req.token;
+
+    logout(token);
+    res.status(204).end();
 });
 
 module.exports = {
