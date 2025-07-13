08305c90adc10a2927acb882861d0e40b6db3430
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-07-13 19:57:18 +0300

    Update index.js file for test environment

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express'); 
 const { mongooseInit } = require('./config/mongooseInit');
 
+let app;
+
 startServer();
 
 async function startServer(){
-    const app = express();
+    app = express();
     require('dotenv').config();
 
-    await mongooseInit(); 
+    if(process.env.NODE_ENVIRONMENT !== 'test'){
+        await mongooseInit(); 
+    }
 
     require('./config/expressInit')(app);
-}
+} 
+
+module.exports = app;
