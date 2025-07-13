d9b89166c61f1a246a90290aaaddfcf415f4e4af
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-07-13 19:56:31 +0300

    Updated config.js file for test environment

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,11 @@ const config = {
         dbURL: "mongodb://localhost:27017/calendar",
         origin: ["http//localhost:5173"]
     },
+    test: {
+        port: process.env.PORT || 3030,
+        dbURL: "mongodb://localhost:27017/calendar",
+        origin: ["http//localhost:5173"]
+    },
     production: {
         port: process.env.PORT || 3000,
         dbURL: process.env.DB_URL_CREDENTIALS,
